b78e0a84e0cd435a6d9ec84e25d213e17e96cae4
milim2 <leemil@sheridancollege.ca>
2019-05-14 19:01:40 -0400

    user register with SALt

--- server/models/user.js
+++ server/models/user.js
@@ -72,15 +72,19 @@ userSchema.pre('save', function(next){
 userSchema.pre('save', function(next) {
     var user = this; // user: userSchema's user, not the function
 
-    bcrypt.genSalt(SALT_I, function(err, salt){
+    if(user.isModified('password')){
+        bcrypt.genSalt(SALT_I, function(err, salt){
         if(err) return next(err);
 
-        bcrypt.hash(user.password, salt, function(err, hash){
+            bcrypt.hash(user.password, salt, function(err, hash){
             if(err) return next(err);
             user.password = hash;
             next();
+            })
         })
-    })
+    } else {
+        next();
+    }
 }) 
 
 
